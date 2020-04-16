8825763febf88f1a1e185a9e70165afdde7f8808
Hubert Wojcik <hwojcikk@gmail.com>
2020-04-16 21:36:08 +0200

    create basics product page

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,8 +1,28 @@
 import React from "react";
-import { FlatList } from "react-native";
+import { FlatList, Text } from "react-native";
+import { useSelector } from "react-redux";
+import ProductItem from "../../components/shop/ProductItem";
 
 const ProductsOverviewScreen = (props) => {
-	return <FlatList />;
+	const products = useSelector((state) => state.products.availableProducts);
+	return (
+		<FlatList
+			data={products}
+			renderItem={(itemData) => (
+				<ProductItem
+					image={itemData.item.imageUrl}
+					title={itemData.item.title}
+					price={itemData.item.price}
+					onViewDetail={() => {}}
+					onAddToCart={() => {}}
+				/>
+			)}
+		/>
+	);
+};
+
+ProductsOverviewScreen.navigationOptions = {
+	headerTitle: "All Products",
 };
 
 export default ProductsOverviewScreen;
--- store/reducers/products.js
+++ store/reducers/products.js
@@ -1,6 +1,7 @@
+import PRODUCTS from "../../data/dummy-data";
 const initialState = {
-	avaibleProducts: [],
-	userProducts: [],
+	availableProducts: PRODUCTS,
+	userProducts: PRODUCTS.filter((prod) => prod.ownerId === "u1"),
 };
 
 export default (state = initialState, action) => {
