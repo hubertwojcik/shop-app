555372d8838123233d5f01214dbffb8446d721c6
Hubert Wojcik <hwojcikk@gmail.com>
2020-04-17 09:49:04 +0200

    add second screen to stack

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -1,12 +1,16 @@
 import { createStackNavigator } from "react-navigation-stack";
 import { createAppContainer } from "react-navigation";
 import { Platform } from "react-native";
+
 import ProductsOverviewScreen from "../screens/shop/ProductsOverviewScreen";
+import ProductDetailScreen from "../screens/shop/ProductDetailScreen";
 
 import Colors from "../constans/Colors";
+
 const ProductsNavigator = createStackNavigator(
 	{
 		ProductsOverview: ProductsOverviewScreen,
+		ProductDetail: ProductDetailScreen,
 	},
 	{
 		defaultNavigationOptions: {
